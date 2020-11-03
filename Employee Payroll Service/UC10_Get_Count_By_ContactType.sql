e77fefe45651c37db770a275222ae4c82590a08d
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-03 13:34:26 +0530

    [Mukhesh].Add New Entities in Employee Payroll Service Database

--- Employee Payroll Service/UC10_Get_Count_By_ContactType.sql
+++
@@ -1,4 +0,0 @@
-use addressBookService;
-update address_book set AddressBookName='Mukhesh',ContactType='Home' where FirstName='Mukhesh';
-/*Get count*/
-select ContactType,Count(ContactType) from address_book group by ContactType;
